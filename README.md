f99ee0ba7c3bc15625ffd2166c5d79353bb623ff
rexybit <email@example.com>
2024-08-24 16:44:41 +0200

    Improvments over original plugin

--- README.md
+++ README.md
@@ -1,10 +1,37 @@
 # Cache Chunker
 
-If you naively trim the oldest messages from context, you'll have to rebuild the cache every message once you hit the context limit. This extension instead trims N messages at a time, so you only have to rebuild the cache every N messages.
+If you naively trim the oldest messages from context, you'll have to rebuild the cache every message once you hit the context limit. This extension instead trims N messages at a time, so you only have to rebuild the cache every N messages. This won't work in group chats and neither if you use automatic summery creation, as these features cause the beginning of the context to change.
+
+## Install
+git clone into your extension directory of ST, e.g. "data\default-user\extensions".
+
+You can also download this repository as zip and paste the contents under e.g."data\default-user\extensions\SillyTavern-CacheChunker"
 
 ## Configuration
 
 There are two settings you can configure for this extension:
 
-- Chunk Size: The number of messages to trim at a time. Default is 10.
-- Max Message History Context: This is the maximum length of the messages part of the context. This should be roughly `max context length - character card length`. I'd like to automate this in the future.
+- Chunk Size: The number of messages to trim at a time. This isn't an exact number, e.g. larger than average messages cause the context to be chunked sooner. Note that one turn (user - assistant) consists of two messages.
+- Max Message History Context: This is the maximum length of the messages part of the context. This should be roughly `max context length - (everything else, character card length, world info, authors note etc`. If you see the context still be revaluated every time, try setting this lower.
+
+## Common Errors
+#### Error: settings.html not found
+This means that the folder name of this plugin under the extensions dir isn't correct. The Folder has to have the same name as the variable `MODULE_NAME` in `index.js`. You can change either one.
+
+## Console output
+This script dumps some Info into the console on each prompt send to the LLM backend:
+
+- Trimming context, message count before: x
+    - Number of Messages in the whole chat
+- Cache circa filled: x out of N 
+    - Indication on when the next whole context evaluation will happen, the closer x is to N, the more likely it is that the next message will cause a complete context reevaluation 
+- Cut messages up to: y
+    - How many messages, starting from the beginning of the chat, are not included in the context
+- Trimming context; message count / context length are now: z / a
+    - z Messages are part of the context, these have a approximated length of a tokens
+
+## Disclaimer
+This is a fork of https://github.com/Omegastick/SillyTavern-CacheChunker
+
+Original credit goes to Omegastick.
+I've improved the Context chunking calculation so that the configured parameters are easier to dial in, to reduce the execution cost of the script and to improve the experience with instruct mode. Also i've expanded the documentation a bit
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import { saveSettingsDebounced } from '../../../../script.js';
 import { extension_settings, renderExtensionTemplate } from '../../../extensions.js';
-import { getTokenCount, guesstimate } from '../../../tokenizers.js';
+import { getTokenCount } from '../../../tokenizers.js';
+import { getContext } from "../../../extensions.js";
+import { power_user } from '../../../power-user.js';
 
-const MODULE_NAME = 'CacheChunker';
+
+const MODULE_NAME = 'SillyTavern-CacheChunker';
 
 const settings = {
     enabled: true,
-
-    chunkSize: 2,
+    chunkSize: 10,
     maxMessageHistoryContext: 2000,
 };
 
@@ -22,46 +24,42 @@ function trimContext(chat) {
     }
 
     console.debug('Trimming context, message count before:', chat.length);
-
-    const startingPoint = getStartingPoint(chat, settings.maxMessageHistoryContext, settings.chunkSize);
-    console.debug('Starting point:', startingPoint);
-    chat.splice(0, startingPoint);
-
-    while (getTokenCount(renderChat(chat)) > settings.maxMessageHistoryContext) {
-        chat.splice(0, settings.chunkSize);
-    }
-
-    console.debug('Trimming context, message count after:', chat.length);
+    const cutPoint = getCuttingPoint(chat, settings.maxMessageHistoryContext, settings.chunkSize);
+    console.debug('Cut messages up to:', cutPoint);
+    chat.splice(0, cutPoint);
+    console.debug('Trimming context; message count / context length are now: ' + chat.length + " / " + getTokenCount(renderChat(chat)));
 }
 
 function renderChat(chat) {
     return chat.map((message) => {
-        return message.mes;
-    }).join('\n');
+        const messagePrefix = message.is_user ? power_user.instruct.input_sequence : power_user.instruct.output_sequence ?? '';
+        const messagePostfix = message.is_user ? power_user.instruct.input_suffix : power_user.instruct.output_suffix ?? '';
+        const formattedMessage = messagePrefix + message.name + ": " + message.mes + messagePostfix;
+        return formattedMessage;
+    }).join("");
 }
 
 /**
- * Uses guesstimate to get a starting point for the chunking process.
+ * Get the index up to which messages in the chat array have to be cut
  * @param {object[]} chat Array of chat messages
  * @param {number} maxTokens Maximum number of tokens to allow in the chat array
  * @param {number} chunkSize Number of messages to remove at a time
- * @param {number} tolerance Amount added to the estimated starting point as a percentage of the total message count
- * @returns {number} Index of the starting point for the chunking process
+ * @returns {number} Index of the point for the chunking process
  */
-function getStartingPoint(chat, maxTokens, chunkSize, tolerance = 0.1) {
-    const totalTokens = guesstimate(renderChat(chat));
-    const target = maxTokens + (maxTokens * tolerance);
+function getCuttingPoint(chat, maxTokens, chunkSize) {
+    const totalTokens = getTokenCount(renderChat(chat));
 
-    if (totalTokens <= target) {
+    const contextPercentageToKeep = (maxTokens / totalTokens);
+    if (contextPercentageToKeep > 1) {
         return 0;
     }
 
-    const targetAsPercentage = target / totalTokens;
-    const targetWithTolerance = targetAsPercentage + tolerance;
-    const index = Math.floor(chat.length * targetWithTolerance);
-    const chunkedIndex = index - (index % chunkSize);
+    const indicesToKeep = Math.floor(chat.length * contextPercentageToKeep);
+    const indexToDeleteUpTo = chat.length - indicesToKeep + 1;
 
-    return chunkedIndex;
+    const chunkedIndexToDeleteUpTo = indexToDeleteUpTo - (indexToDeleteUpTo % chunkSize) + chunkSize;
+    console.debug("Cache circa filled: " + indexToDeleteUpTo % chunkSize + ' out of ' + chunkSize)
+    return chunkedIndexToDeleteUpTo;
 }
 
 window['CacheChunker_trimContext'] = trimContext;
